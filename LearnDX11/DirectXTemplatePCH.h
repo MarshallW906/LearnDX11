b64dc32f7bb40215c9af9260f156599ac912b9d9
MarshallW906 <MaticWang@hotmail.com>
2021-01-22 16:07:17 -0800

    Code base learned from https://www.3dgep.com/introduction-to-directx-11/

---
+++ LearnDX11/DirectXTemplatePCH.h
@@ -0,0 +1,31 @@
+// System Includes
+#include <Windows.h>
+
+// DX11
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+// STL
+#include <iostream>
+#include <string>
+
+// Link library dependencies
+// I set them in project settings
+// consider uncommenting them when there are problems
+//#pragma comment(lib, "d3d11.lib")
+//#pragma comment(lib, "dxgi.lib")
+//#pragma comment(lib, "d3dcompiler.lib")
+//#pragma comment(lib, "winmm.lib")
+
+// Safely Release
+template <typename T>
+inline void SafeRelease(T& ptr)
+{
+	if (ptr != NULL)
+	{
+		ptr->Release();
+		ptr = NULL;
+	}
+}
---
+++ LearnDX11/SimplePixelShader.hlsl
@@ -0,0 +1,9 @@
+// Pixel
+struct PixelShaderInput
+{
+	float4 color: COLOR;
+};
+
+float4 SimplePixelShader(PixelShaderInput IN) : SV_TARGET{
+	return IN.color;
+}
